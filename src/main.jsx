a49c87d6de125e940532b5b408828f4c86bdc561
emild <emildagsberg@hotmail.dk>
2025-12-12 13:45:56 +0100

    got rid of app.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+
+        {/* Default route inside App */}
+        <Route index element={<Home />} />
+
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
