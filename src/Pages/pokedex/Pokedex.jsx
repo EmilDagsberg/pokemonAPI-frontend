f27818bf1dba44ec3b6f7807a0af1b48d24a1f6f
Emil Kriegel <emilkriegel@gmail.com>
2025-12-17 12:36:50 +0100

    Initial Pokedex commit

--- src/Pages/pokedex/Pokedex.jsx
+++ src/Pages/pokedex/Pokedex.jsx
@@ -1,12 +1,66 @@
-import { useEffect, useState } from "react"
-import facade from "../apiFacade.js"
+import { useEffect, useState } from "react";
+import facade from "../apiFacade.js";
 
-export default function Pokedex () {
-    const [pokeDex, setPokedex] = useState([]);
+export default function Pokedex() {
+  const [pokedex, setPokedex] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const API_URI = "http://localhost:7070/api/pokedex/" //OBS mangler i backend
+  /*     useEffect(() => {
+        const API_URI = "http://localhost:7070/api/pokedex/"
 
-        
-    })
+        facade.fetchPokedex()
+        .then((data) => {
+          setPokedex(data);
+        })
+        .catch((error) => {
+        console.error("Error while fetching Pokedex data", error);
+      });
+    }, []) */
+
+  useEffect(() => {
+    // Mock data. Ovenstående useEffect bruges når backend virker.
+    const mockPokedex = [
+      { pokemonId: 1, userId: 1, isOnTeam: true },
+      { pokemonId: 25, userId: 1, isOnTeam: false },
+      { pokemonId: 6, userId: 1, isOnTeam: true },
+      { pokemonId: 4, userId: 1, isOnTeam: false },
+      { pokemonId: 7, userId: 1, isOnTeam: true },
+      { pokemonId: 150, userId: 1, isOnTeam: false },
+    ];
+
+    setPokedex(mockPokedex);
+    setLoading(false);
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div className="pokedex-container">
+      <h1>Your pokedex</h1>
+      <div className="pokemon-grid">
+        {pokedex.map((pokemon) => (
+          <div key={pokemon.id} className="pokemon-card">
+            <img src={pokemon.sprite} alt={pokemon.name} />
+            <h3>Name: {pokemon.name}</h3>
+            <div className="locations">
+              <strong>Locations:</strong>
+              <ul>
+                {pokemon.locations.map((location, index) => (
+                  <li key={index}>{location}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <strong>Types:</strong>
+              <ul>
+                {pokemon.types.map((type, index) => (
+                  <li key={index}>{type}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
