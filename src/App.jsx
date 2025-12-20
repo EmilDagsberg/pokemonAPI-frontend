6388def1bc0f4e491b703c9f18b9dac513e3bbc6
emild <emildagsberg@hotmail.dk>
2025-12-20 13:41:03 +0100

    fixed pokedex and header

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Header from './components/header/Header'
 
 function App() {
   
-      const headers = [
+       const headers = [
         { title: "Home", url: "/" },
         { title: "Pokedex", url: "/pokedex" },
         { title: "Vision", url: "/vision" }, 
         { title: "Endpoints", url: "/endpoint-overview", 
             children: [
                 { title: "Overview", url: "/endpoint-overview"},
-                { title: "Search Pokemon", url: "/search-pokemon"}
+                { title: "Search Pokemon", url: "/search-pokemon"},
+                { title: "Get Pokemon by type", url: "/get-pokemon-by-type"},
+                { title: "Get a random Pokemon", url: "/get-random-pokemon"}
             ],
         },
     ];
--- src/Pages/pokedex/Pokedex.jsx
+++ src/Pages/pokedex/Pokedex.jsx
@@ -1,22 +1,10 @@
 import { useEffect, useState } from "react";
 import styles from "./Pokedex.module.css";
+import facade from "../../apiFacade";
 
 import Header from "../../components/header/Header";
 
 export default function Pokedex() {
-    const headers = [
-        { title: "Home", url: "/" },
-        { title: "Pokedex", url: "/pokedex" },
-        { title: "Vision", url: "/vision" }, 
-        { title: "Endpoints", url: "/endpoint-overview", 
-            children: [
-                { title: "Overview", url: "/endpoint-overview"},
-                { title: "Search Pokemon", url: "/search-pokemon"},
-                { title: "Get Pokemon by type", url: "/get-pokemon-by-type"},
-                { title: "Get a random Pokemon", url: "/get-random-pokemon"}
-            ],
-        },
-    ];
   const [pokedex, setPokedex] = useState([]);
   const [loading, setLoading] = useState(true);
   const [showOnlyTeam, setShowOnlyTeam] = useState(false);
