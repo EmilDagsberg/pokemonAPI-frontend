7c4e44396b0f236e7bece770ffd09021bd2570e0
AndreasKuke <marslevandreas2@gmail.com>
2025-12-20 12:35:51 +0100

    Added stuff for vision page and some nice design changes

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,30 +1,14 @@
 import styles from "./Home.module.css"
-import Header from "../../components/header/Header"
 import logo from "../../assets/PokemonApiLogo.png"
 import darkmode from "../../assets/darkmode.png"
 const Home = () => {
-    const headers = [
-        { title: "Home", url: "/" },
-        { title: "Pokedex", url: "/pokedex" },
-        { title: "Vision", url: "/vision" }, 
-        { title: "Endpoints", url: "/endpoint-overview", 
-            children: [
-                { title: "Overview", url: "/endpoint-overview"},
-                { title: "Search Pokemon", url: "/search-pokemon"}
-            ],
-        },
-    ];
-
     return (
-        <div className={styles.background}>
-            <Header headers={headers} />
-            <div className={styles.container}>
-                <div className={styles.logoSection}>
-                    <img src={logo} alt="Pokeball Logo" className={styles.logo} />
-                </div>
-                <h2 className={styles.welcome}>Welcome to the PKMON API</h2>
-                <h2 className={styles.welcome}>Refer to the header for usage</h2>
+        <div className={styles.container}>
+            <div className={styles.logoSection}>
+                <img src={logo} alt="Pokeball Logo" className={styles.logo} />
             </div>
+            <h2 className={styles.welcome}>Welcome to the PKMON API</h2>
+            <h2 className={styles.welcome}>Refer to the header for usage</h2>
         </div>
     )
 }
--- src/Pages/pokedex/Pokedex.jsx
+++ src/Pages/pokedex/Pokedex.jsx
@@ -1,29 +1,12 @@
 import styles from "./Pokedex.module.css";
-import Header from "../../components/header/Header";
 
 const Pokedex = () => {
-  const headers = [
-    { title: "Home", url: "/" },
-    { title: "Pokedex", url: "/pokedex" },
-    { title: "Vision", url: "/vision" },
-    {
-      title: "Endpoints",
-      url: "/endpoint-overview",
-      children: [
-        { title: "Overview", url: "/endpoint-overview" },
-        { title: "Search Pokemon", url: "/search-pokemon" },
-      ],
-    },
-  ];
-
   return (
-    <div className={styles.background}>
-      <Header headers={headers} />
-
+    <>
       {/* PUT MEAT IN HERE */
       
       }
-    </div>
+    </>
   )
 };
 
